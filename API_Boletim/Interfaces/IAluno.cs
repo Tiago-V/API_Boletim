8692cda2b459d636c42547353893474592ae28a9
unknown <tiagocvalim@gmail.com>
2020-09-01 10:21:50 -0300

    Feito

---
+++ API_Boletim/Interfaces/IAluno.cs
@@ -0,0 +1,19 @@
+﻿using API_Boletim.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Boletim.Interfaces
+{
+    interface IAluno
+    {
+        // CRUD -
+
+        List<Aluno> ListarTodos();
+        Aluno BuscarPorId(int id);
+        Aluno Cadastrar(Aluno a);
+        Aluno Alterar(Aluno a);
+        Aluno Remover(Aluno a);
+    }
+}
---
+++ API_Boletim/Repositories/AlunoRepository.cs
@@ -0,0 +1,37 @@
+﻿using API_Boletim.Domains;
+using API_Boletim.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Boletim.Repositories
+{
+    public class AlunoRepository : IAluno
+    {
+        public Aluno Alterar(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Cadastrar(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Aluno> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Remover(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
